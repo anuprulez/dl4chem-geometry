b4445668e8c962565b0bf578fbd2e7c2170b22f7
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2020-01-20 15:12:20 +0100

    refactor

--- refactored_mol_prediction/run_cod_featurize.sh
+++ refactored_mol_prediction/run_cod_featurize.sh
@@ -1,1 +1,1 @@
-python COD_featurize.py --loaddir data/ --savedir data/ --loadfile data/CODconh25.p --num_mol 50
+python COD_featurize.py --loaddir data/ --savedir data/ --loadfile data/CODconh25.p --num_mol 1000
--- refactored_mol_prediction/run_training.sh
+++ refactored_mol_prediction/run_training.sh
@@ -1,1 +1,1 @@
-python PredX_train.py --data COD --mpnn_steps 5 --num_epochs 10 --loaddir checkpoints/model.ckpt
+python PredX_train.py --data COD --mpnn_steps 5 --num_epochs 50 --loaddir checkpoints/model.ckpt
